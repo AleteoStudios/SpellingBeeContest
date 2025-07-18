7c153a2449f49da8ff13f86c8177bd839f1c9782
gilorock86 <gilorock86@gmail.com>
2025-07-18 00:05:56 -0600

    Add Panel Settings

---
+++ Assets/Scripts/Settings/DisplayManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        for(int i = 0; 1<Display.displays.Length; ++i)
+        {
+            Display.displays[i].Activate(1920,1080,60);
+        }
+    }
+
+   
+}
