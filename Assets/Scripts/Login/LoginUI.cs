96da549e23296c256d3318cb003539bbc8c8a7da
gilorock86 <gilorock86@gmail.com>
2025-08-15 01:28:11 -0600

    added login system

---
+++ Assets/Scripts/Login/LoginUI.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginUI : MonoBehaviour
+{
+    [SerializeField] TMP_InputField emailInput;
+    [SerializeField] TMP_InputField passInput;
+    [SerializeField] TextMeshProUGUI feedback;
+    [SerializeField] GameObject panelClose;
+
+    public void OnClickLogin()
+    {
+        feedback.text = "Conectando...";
+        StartCoroutine(AuthSupabase.Login(
+            emailInput.text.Trim(),
+            passInput.text,
+            onOk: () => {
+                feedback.text = "✅ Acceso concedido";
+                panelClose.SetActive(false);
+            },
+            onError: e => feedback.text = $"❌ {e}"
+        ));
+    }
+
+    public void OnClickLogout()
+    {
+        AuthSupabase.Logout();
+        feedback.text = "Sesión cerrada.";
+    }
+}
